f04852e9d59dc6890b90ce5ea5f4f1538531f61a
Deborah <deborahsong7@gmail.com>
2024-12-18 15:40:58 +0100

    pad

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -17,7 +17,7 @@ const Search = () =>{
     <section className=" bg-brown pt-16">
    <div className="bg-offwhite relative isolate px-6 pt-1">
     <h2 className="text-3xl font-semibold tracking-tight text-brown pt-20 text-center sm:text-4xl">PROPERTIES FOR SALE</h2>
-    <div className=" mx-auto max-w-2xl px-2 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-16 lg:py-8">
+    <div className=" mx-auto max-w-2xl px-2 py-16 sm:px-6 sm:py-20 lg:max-w-7xl lg:px-16 lg:py-8">
         <div className="grid container  grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-12">
         { filterProperties.length > 0 ? (filterProperties.map((property)=>(
        <PropertyCard key={property.id} property= {property}/>
