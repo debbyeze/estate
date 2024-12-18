bb7d4bae1dfdad6220352bbc0685447132a96cc4
Deborah <deborahsong7@gmail.com>
2024-12-18 15:11:04 +0100

    px

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -84,7 +84,7 @@ export default function Overview() {
           <h3 className="text-2xl font-semibold tracking-tight text-brown pt-20 text-start sm:text-3xl uppercase px-8">
             Featured {house.type}s
           </h3>
-            <div className=" mx-auto max-w-2xl px-8 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-16 lg:py-8">
+            <div className=" mx-auto max-w-2xl px-2 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-16 lg:py-8">
               <div className="grid container  grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-12">
                 {featuredProperties.length > 0 ? (
                   featuredProperties
